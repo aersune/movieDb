53adac70ec4dc228e643cac1f5a6a4cffd54cbb4
aersune <aersune@gmail.com>
2024-12-09 14:02:30 +0500

    user data

--- lib/presentation/ui/widgets/user_profile_widget.dart
+++ lib/presentation/ui/widgets/user_profile_widget.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_db/domain/models/user_data_model.dart';
 import 'package:movie_db/domain/provider.dart';
 import 'package:movie_db/presentation/components/app_colors.dart';
 import 'package:movie_db/presentation/components/app_style.dart';
@@ -11,26 +13,34 @@ class UserProfileWidget extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final prov = context.watch<MoviesProvider>();
+    var image = prov.userData.avatar?.tmdb?.avatarPath;
     return Row(
       children: [
         Container(
           width: 70,
           height: 70,
-          decoration:  BoxDecoration(
+          decoration: BoxDecoration(
               shape: BoxShape.circle,
               border: Border.all(color: AppColors.whiteColor),
-              image: const DecorationImage(
-                  image: AssetImage(
-                      "assets/profile.png"),
-              fit: BoxFit.cover
-              )),
+              image: DecorationImage(
+                  image: image != null
+                      ? CachedNetworkImageProvider("https://image.tmdb.org/t/p/w500$image")
+                      : const AssetImage("assets/profile.png") as ImageProvider,
+                  fit: BoxFit.cover)),
         ),
         const SizedBox(width: 20),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("${prov.userData.username}", style: AppStyle.titleStyle.copyWith(fontWeight: FontWeight.w400),),
-            Text('useremail@gmail.com', style: AppStyle.normalStyle.copyWith(fontWeight: FontWeight.w400, color: AppColors.grayText),),
+            Text(
+              prov.userData.name ?? 'user',
+              style: AppStyle.titleStyle.copyWith(fontWeight: FontWeight.w400),
+            ),
+            Text(
+              prov.userData.username ?? 'username...',
+              style: AppStyle.normalStyle.copyWith(
+                  fontWeight: FontWeight.w400, color: AppColors.grayText),
+            ),
           ],
         )
       ],
